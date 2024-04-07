d547479bc59331cf54ba120254ee46e9a00eb238
Supun Thilakshana Dharmarathna <82252921+SupunTd@users.noreply.github.com>
2024-04-07 11:14:36 +0530

    update xml file

--- part1.py
+++ part1.py
@@ -9,6 +9,7 @@ image[60:80, 60:80] = 200  # Object 2
 # Add Gaussian noise
 noisy_image = image + np.random.normal(loc=0, scale=15, size=image.shape).astype(np.uint8)
 
+
 # Implement Tsutomu's algorithm
 _, thresholded_image = cv2.threshold(noisy_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
